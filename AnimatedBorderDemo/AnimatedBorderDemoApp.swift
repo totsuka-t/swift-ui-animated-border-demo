59855bb22b0bfb20f82c1ac5b0242262227b08b7
totsuka <putti0702@gmail.com>
2023-12-22 23:13:12 +0900

    first commit

---
+++ AnimatedBorderDemo/AnimatedBorderDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedBorderDemoApp.swift
+//  AnimatedBorderDemo
+//  
+//  Created by user on 2023/12/22
+//
+
+
+import SwiftUI
+
+@main
+struct AnimatedBorderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedBorderDemo/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  AnimatedBorderDemo
+//  
+//  Created by user on 2023/12/22
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
